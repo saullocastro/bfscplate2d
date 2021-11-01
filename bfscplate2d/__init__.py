434bc3f7395bcdcc1688cc61262fd2e120aa4068
saullocastro <saullogiovani@gmail.com>
2021-11-01 16:45:29 +0100

    DEV: nonlinear matrices implementation

--- bfscplate2d/__init__.py
+++ bfscplate2d/__init__.py
@@ -4,7 +4,7 @@ BFSCPLATE2D - Implementation of the BFSC plate finite element in 2D
 Author: Saullo G. P. Castro
 
 """
-from .bfscplate2d import BFSCPlate2D, update_KC0, update_KG, update_KG_cte_N, update_M
-from .bfscplate2d import INT, DOUBLE, KC0_SPARSE_SIZE, KG_SPARSE_SIZE, M_SPARSE_SIZE
+from .bfscplate2d import BFSCPlate2D, update_KC0, update_KCNL, update_KG, update_KG_cte_N, update_M, update_fint
+from .bfscplate2d import INT, DOUBLE, KC0_SPARSE_SIZE, KCNL_SPARSE_SIZE, KG_SPARSE_SIZE, M_SPARSE_SIZE
 DOF = 10
 
