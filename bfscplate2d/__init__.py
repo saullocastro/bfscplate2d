de0262e2c1bdcb3aad24c4562baace632bcfb70f
Saullo G. P. Castro <saullogiovani@gmail.com>
2023-04-12 12:33:49 +0200

    BUG: fixed int long long long issue

--- bfscplate2d/__init__.py
+++ bfscplate2d/__init__.py
@@ -4,8 +4,13 @@ BFSCPLATE2D - Implementation of the BFSC plate finite element in 2D
 Author: Saullo G. P. Castro
 
 """
+import numpy as np
+
 from .bfscplate2d import BFSCPlate2D, update_KC0, update_KCNL, update_KG, update_KG_cte_N, update_M, update_fint
-from .bfscplate2d import DOUBLE, KC0_SPARSE_SIZE, KCNL_SPARSE_SIZE, KG_SPARSE_SIZE, M_SPARSE_SIZE
+from .bfscplate2d import KC0_SPARSE_SIZE, KCNL_SPARSE_SIZE, KG_SPARSE_SIZE, M_SPARSE_SIZE
+
+
 INT = int
+DOUBLE = np.float64
 DOF = 10
 
